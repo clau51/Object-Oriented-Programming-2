dc23195a46ec94267079493ba33b99ffb25a16cf
Carmen Lau <clau51@myseneca.ca>
2023-01-31 22:29:11 -0500

    rough p1

---
+++ WS03/p1/Collection.h
@@ -0,0 +1,66 @@
+#ifndef SDDS_COLLECTION_H_
+#define SDDS_COLLECTION_H_
+
+#include <iostream>
+#include <iomanip>
+#include "Pair.h"
+
+namespace sdds
+{
+   template<typename T, unsigned int capacity>
+   class Collection
+   {
+      T m_collection[capacity]{}; //Pair m_collection[10]
+      unsigned int m_noOfElements{};
+      T m_dummy{}; //Pair m_dummy - call constructor & initialize to 0
+
+
+
+   public:
+      unsigned int size() const
+      {
+         return m_noOfElements;
+      }
+
+      void display(std::ostream& ostr = std::cout) const
+      {
+         ostr << "----------------------" << std::endl;
+         ostr << "| Collection Content |" << std::endl;
+         ostr << "----------------------" << std::endl;
+
+         for (unsigned i = 0; i < m_noOfElements; i++)
+         {
+            ostr << m_collection[i] << std::endl;
+         }
+
+         ostr << "----------------------" << std::endl;
+      }
+
+      bool add(const T& item)
+      {
+         bool added = false;
+         if (m_noOfElements < capacity)
+         {
+            m_collection[m_noOfElements] = item;
+            m_noOfElements++;
+            added = true;
+         }
+         return added;
+      }
+
+      T operator[](unsigned int index) const
+      {
+         return index < m_noOfElements ? m_collection[index] : m_dummy;
+      }
+   };
+
+   template<>
+   class Collection<Pair, 100>
+   {
+      Pair m_dummy{ "No key", "No Value" };
+   };
+
+}
+
+#endif
+

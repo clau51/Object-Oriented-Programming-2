9760de9392793793ad19cc71d97fb5d0021e32d6
Carmen Lau <clau51@myseneca.ca>
2023-02-08 00:03:23 -0500

    child.h attributes done

--- WS04/p2/Child.h
+++ WS04/p2/Child.h
@@ -1,10 +1,18 @@
 #ifndef SDDS_CHILD_H_
 #define SDDS_CHILD_H_
 
+#include <string>
+#include "Toy.h"
+
 namespace sdds
 {
    class Child
    {
+      std::string name{};
+      int age{};
+      const Toy** toy{};
+   public:
+      Child(std::string name, int age, const Toy* toys[], size_t count)
    };
 }
 
